fc7154027e041d62c3c0034f98a138df678b83d7
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-08-30 17:13:47 +0530

    user,trip schema completed

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
-import express from "express";
+
 import mongoose from "mongoose";
+import express from "express";
 import nodeMailer from "nodemailer";
 import cors from "cors";
 import axios from "axios";
-import App from "../App";
+
 
 
 const app = express();
@@ -14,7 +15,18 @@ app.use(cors());
 app.use(express.json());
 
 const mongoUri = "mongodb+srv://piyush:Piyush@cluster0.wabcbqp.mongodb.net/";
-mongoose.connect()
+mongoose.connect(mongoUri).then(() => {
+   console.log("Connected to mongoDb")
+}).catch(err => {
+   console.log("Mongodb connection error" , err);
+   process.exit(1);
+})
+
+
+app.listen(port , () => {
+   console.log("Server is running at port 3000");
+})
+
 
 
 
