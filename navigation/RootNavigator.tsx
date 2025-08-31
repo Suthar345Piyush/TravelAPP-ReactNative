7f4114ff7b97de195bd372dc2cddf6f4a0e0ab01
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-08-31 16:30:10 +0530

    worked on rootnavigator file

---
+++ navigation/RootNavigator.tsx
@@ -0,0 +1,47 @@
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './TabNavigator';
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+
+
+export type RootStackParamList = {
+   Auth : undefined;
+   SignIn : undefined;
+   SignUp : undefined;
+   Main : undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+
+export default function RootNavigator(){
+    // const {isLoaded , isSignedIn}  = useAuth();
+
+    // loading till the clerk authentication , working... 
+
+    if(!isLoaded){
+       return (
+         <View style={{flex :1 , justifyContent : "center" , alignItems : "center"}}>
+           <ActivityIndicator size="large" color="#FF5722"/>
+         </View>
+       );
+    }
+
+     return (
+       <Stack.Navigator screenOptions={{headerShown : false}}>
+         {isSignedIn ? (
+            <Stack.Screen name="Main" component={TabNavigator}/>
+         ) : (
+           <Stack.Group>
+             <Stack.Screen name="SignIn" component={SignInScreen}/>
+             <Stack.Screen name="SignUp" component={SignUpScreen}/>
+           </Stack.Group>
+         )}
+       </Stack.Navigator>  
+     );
+};
+
+
+
