776def7c221ce4450d401c963c6f69e616dffdfa
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-08-31 23:36:03 +0530

    almost done with signin,signup screen work

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import {NavigationContainer} from "@react-navigation/native";
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css";
 import TabNavigator from './navigation/TabNavigator';
+import RootNavigator from "./navigation/RootNavigator";
 
 export default function App() {
   return (
      <NavigationContainer>
-       <TabNavigator />
+       <RootNavigator />
      </NavigationContainer>
   );
 }
