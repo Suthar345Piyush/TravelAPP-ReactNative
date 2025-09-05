07500ab31c930efe2512272ca5824817b140f2f1
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-09-05 12:32:37 +0530

    i have no idea what i am doing

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import {Text , View , SafeAreaView , StyleSheet ,Pressable} from "react-native";
+import {Text , View , SafeAreaView , StyleSheet ,Pressable, ScrollView, Image, TouchableOpacity} from "react-native";
 import React, {useCallback, useState} from "react";
 import { useClerk, useUser } from "@clerk/clerk-expo";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
@@ -6,7 +6,7 @@ import { HomeStackParamList } from "./HomeScreen";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import axios from "axios";
 import dayjs from "dayjs";
-
+import {Ionicons} from "@expo/vector-icons";
 
 
 export type TabNavigatorParamList = {
@@ -82,34 +82,78 @@ const ProfileScreen = () => {
 
     const email = user.primaryEmailAddress?.emailAddress || "No email found";
     const name = user.fullName || "Anonymous user";
-    
-
-
-
-
-
-
-
-
-
-
-
+    const handle = `@${user.username || user.id.slice(0 , 8)}`;
 
 
   const handleSignout = async () => {
         try {
           await signOut();
         } catch (err) {
-          console.log("Error" , err);
+          console.error("Sign-out error: " , JSON.stringify(err , null , 2));
         }
-    }
+    };
+
+
 
     return (
-       <SafeAreaView>
-         <Text>ProfileScreen</Text>
-         <Pressable onPress={handleSignout}>
-           <Text>Logout</Text>
-         </Pressable>
+       <SafeAreaView className="flex-1 bg-white">
+        <ScrollView className="flex-1" contentContainerStyle={{paddingBottom : 100}}>
+            <View className="relative bg-pink-100 items-center pb-6 rounded-b-3xl">
+               <View className="bg-yellow-500 absolute top-4 left-4 rounded-full py-1 px-3">
+                 <Text className="text-xs text-white font-semibold">
+                  PRO
+                 </Text>
+               </View>
+
+               {/* profile image  section  */}
+
+               <View className="mt-8 relative">
+                  <Image source={{uri : profileImage}} className="w-24 h-24 rounded-full"/>
+                    <TouchableOpacity className="absolute -bottom-1 -right-1 bg-white p-1 rounded-full border border-gray-300">
+                     <Ionicons name="pencil" size={12} color="#555"/>
+                   </TouchableOpacity>
+               </View>
+
+
+               {/* name , account handle , email  */}
+             <Text className="mt-3 text-lg font-semibold">{name}</Text>
+             <Text className="text-gray-500">{handle}</Text>
+             <Text className="text-gray-500 text-sm mt-1">{email}</Text>
+
+
+             {/* followers , following  */}
+            <View className="flex-row justify-center mt-4 space-x-12"> 
+               <View className="items-center">
+                <Text className="font-bold text-base">0</Text>
+                 <Text className="text-xs text-gray-500 tracking-wide">FOLLOWERS</Text>
+               </View>
+               <View className="items-center">
+                 <Text className="font-bold text-base">0</Text>
+                 <Text className="text-xs text-gray-500 tracking-wide">FOLLOWING</Text>
+               </View>
+            </View>
+
+
+            {/* button for sign out  */}
+          
+           <TouchableOpacity className="bg-orange-500 px-6 py-3 rounded-lg mt-4" onPress={handleSignout}>
+             <Text className="text-white font-semibold text-base">Sign Out</Text>
+           </TouchableOpacity>
+        </View>
+
+        {/* tabs  */}
+
+        <View>
+           <Text>Trips</Text>
+           <Text>Guides</Text>
+           <TouchableOpacity>
+             
+           </TouchableOpacity>
+        </View>
+
+
+        </ScrollView>
+
        </SafeAreaView>
     );
 };
