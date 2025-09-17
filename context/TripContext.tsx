b5351777043a324d23c21e535d561e6022e29a35
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-09-17 16:30:07 +0530

    worked on trip context file

---
+++ context/TripContext.tsx
@@ -0,0 +1,42 @@
+import React , {createContext , useContext , useState} from "react";
+
+// adding type safety initially 
+
+export type Trip = {
+   id : string;
+   location : string;
+   startDate? : string;
+   endDate?: string;
+   image? : string;
+   places: string[];
+};
+
+
+type TripContextType = {
+   trips : Trip[];
+   addTrip : (trip : Trip) => void;
+};
+
+
+const TripContext = createContext<TripContextType>({
+   trips : [],
+   addTrip : () => {},
+});
+
+export const TripProvider = ({children} : {children : React.ReactNode}) => {
+    const [trips , setTrips] = useState<Trip[]>([]);
+
+    const addTrip = (trip : Trip) => {
+       setTrips((prev) => [trip , ...prev]);
+    };
+
+
+    return (
+       <TripContext.Provider value={{trips , addTrip}}>
+         {children}
+       </TripContext.Provider>
+    );
+};
+
+export const useTrip = () => useContext(TripContext);
+
